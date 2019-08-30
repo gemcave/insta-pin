5c97ff14570d59cc5572e5740dbf8ad9f178a7a3
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-08-30 23:38:17 +0600

    devise install

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'bootstrap', '~> 4.3.1'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem 'devise', '~> 4.7'
+
 group :development do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,9 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+	# config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+	config.action_mailer.default_url_options = { host: 'http://insta-pin.herokuapp.com/', port: 3000 }
+
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
