8855a376339ad0fdc75a117daf121603956e8f12
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-09-05 00:30:39 +0600

    Subscription controller methods and profile view was updated

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,25 +5,33 @@ class ProfilesController < ApplicationController
 	end
 
 	def subscribe 
-		if current_user.subscriptions.exists?(friend_id: @user.id)
-			redirect_to profile_path(@user), notice: "Вы уже подписались на данного пользователя"
+		if current_user.id == @user.id
+			redirect_to profile_path(@user), notice: "Вы не можете подписаться на самого себя"
 		else
-			@subscription = current_user.subscriptions.build
-			@subscription.friend_id = @user_id
-			@subscription.save
-			if @subscription.save
-				redirect_to profile_path(@user), notice: "Вы подписались на данного пользователя"
+			if current_user.subscriptions.exists?(friend_id: @user.id)
+				redirect_to profile_path(@user), notice: "Вы уже подписались на данного пользователя"
+			else
+				@subscription = current_user.subscriptions.build
+				@subscription.friend_id = @user.id
+				@subscription.save
+				if @subscription.save
+					redirect_to profile_path(@user), notice: "Вы подписались на данного пользователя"
+				end
 			end
 		end
 	end
 
 	def unsubscribe 
-		if current_user.subscriptions.exists?(friend_id: @user.id)
-			@subscription = current_user.subscriptions.find_by_friend_id(@user.id)
-			@subscription.destroy
-			redirect_to profile_path(@user), notice: "Вы отписались на данного пользователя"
-		else
-			redirect_to profile_path(@user), notice: "Вы не были подписаны на данного пользователя"
+		if current_user.id == @user.id
+			redirect_to profile_path(@user), notice: "Вы не можете подписаться на самого себя"
+		else 
+			if current_user.subscriptions.exists?(friend_id: @user.id)
+				@subscription = current_user.subscriptions.find_by_friend_id(@user.id)
+				@subscription.destroy
+				redirect_to profile_path(@user), notice: "Вы отписались на данного пользователя"
+			else
+				redirect_to profile_path(@user), notice: "Вы не были подписаны на данного пользователя"
+			end
 		end
 	end
 	
