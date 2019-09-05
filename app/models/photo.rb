be042d78814390efd6469457fcef92e61fe11660
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-09-06 00:18:31 +0600

    name was added and addition for User and Photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ApplicationRecord
 	belongs_to :user
 	has_attached_file :image, styles: { large: "600x600", medium: "300x300>", thumb: "100x100>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+	validates :description,:presence: true
+	validates :image,:presence: true
 end
