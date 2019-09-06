5b12fbdb7b7a3568f3bbc14d8e13c4f711a10add
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-09-06 11:56:38 +0600

    small validation fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
 				 :recoverable, :rememberable, :validatable
 
-	validates :name,:presence: true
+	validates :name, presence: true
 
 	has_many :photos
 	has_many :subscriptions
